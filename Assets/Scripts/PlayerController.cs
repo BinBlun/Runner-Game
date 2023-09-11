8e95e35b1be6805fa2282cf45ba7933156738cbf
Duc Anh <binflinterfler292001@gmail.com>
2023-09-11 17:39:34 +0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true) 
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Runner Game
+ A game in which you dodge the obstacles to save your life. 
